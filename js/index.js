0779bd83c93a1be4e6b8b568d0fb60495f7660c5
SyedAlamin <syedalamin2580@gmail.com>
2023-02-25 21:25:28 +0600

    all

--- js/index.js
+++ js/index.js
@@ -1,19 +1,16 @@
-
-
-
-
-const loadAllData = (searchText) => {
-    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
+// data load 
+const loadAllData = (id) => {
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${id}`;
     fetch(url)
         .then(res => res.json())
         .then(data => displayContainer(data.meals));
 }
-
+// card add 
 const displayContainer = searchData => {
     const searchContainer = document.getElementById('search-container');
     searchContainer.innerHTML = '';
     searchData.forEach(search => {
-        console.log(search);
+        // console.log(search);
         const searchDiv = document.createElement('div');
         searchDiv.innerHTML = ` 
         <div class="  bg-slate-100 shadow-lg flex rounded-md">
@@ -21,26 +18,19 @@ const displayContainer = searchData => {
             <div class=" p-5 w-[60%] ">
                 <h2 class="text-xl font-bold">${search.strMeal.slice(0, 15) + "..."}</h2>
                 <p class="mt-2 ">${search.strInstructions.slice(0,80) + "..."}</p>
-                <button class="text-yellow-500 font-semibold mt-2">View Details</button>
+                <button onClick="idDetail('${search.idMeal}')" class="text-yellow-500 font-semibold mt-2 ">View Details</button>
             </div>
         </div>
         `;
         searchContainer.appendChild(searchDiv);
-
     })
 }
-
+// search bar and button add 
 const searchBtn = () =>{
     const searchText = document.getElementById('text-input-field').value;
     // console.log(searchText)
-    loadAllData(searchText)
-
-    
+    loadAllData(searchText);
 }
-
-
-
-
 
 
 
